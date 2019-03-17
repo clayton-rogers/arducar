587a45c6304037e6ef26fa33a96a616db4d57d55
Clayton Rogers <claytonrogers53@gmail.com>
2019-03-17 18:12:59 -0400

    Ordered pin definitions

--- pins.cpp
+++ pins.cpp
@@ -1,8 +1,9 @@
 #include "pins.h"
 
-const byte PIN_LED = 13;
-const byte PIN_N_SCE = 11;
-const byte PIN_N_DC = 10;
-const byte PIN_N_DATA = 9;
 const byte PIN_N_CLK = 8;
+const byte PIN_N_DATA = 9;
+const byte PIN_N_DC = 10;
+const byte PIN_N_SCE = 11;
 const byte PIN_N_RST = 12;
+
+const byte PIN_LED = 13;
